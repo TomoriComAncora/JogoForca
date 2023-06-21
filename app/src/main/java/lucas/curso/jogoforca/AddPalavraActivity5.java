188a52dc5e2c9e6eab5e1d5ebc651b55111a1fb7
Alkazuz <kerallis2015@gmail.com>
2023-06-21 17:38:17 -0300

    feat: adicionando banco de dados de palavra

--- app/src/main/java/lucas/curso/jogoforca/AddPalavraActivity5.java
+++ app/src/main/java/lucas/curso/jogoforca/AddPalavraActivity5.java
@@ -3,12 +3,33 @@ package lucas.curso.jogoforca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddPalavraActivity5 extends AppCompatActivity {
+
+
+    EditText editTextPalavra;
+    Button buttonSalvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_palavra5);
+
+        editTextPalavra = findViewById(R.id.editTextText);
+        buttonSalvar = findViewById(R.id.button);
+
+        buttonSalvar.setOnClickListener(v -> {
+            String palavra = editTextPalavra.getText().toString();
+            if (palavra.isEmpty()) {
+                Toast.makeText(this, "Digite uma palavra", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Palavra salva", Toast.LENGTH_SHORT).show();
+                MainActivity.getBancoDeDados().inserirPalavra(palavra);
+                finish();
+            }
+        });
     }
 }
